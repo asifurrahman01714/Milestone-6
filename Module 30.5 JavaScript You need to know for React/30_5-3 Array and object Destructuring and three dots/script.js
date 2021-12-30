bc2d8b02ca20bc263dea2af4ff4946493173ecdd
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 15:07:38 +0600

    create object shortcut

--- Module 30.5 JavaScript You need to know for React/30_5-3 Array and object Destructuring and three dots/script.js
+++ Module 30.5 JavaScript You need to know for React/30_5-3 Array and object Destructuring and three dots/script.js
@@ -9,3 +9,13 @@ console.log(restWithoutBracket);
 const person = {name: 'John', age: 30, job: 'developer'};
 const {name, age, job} = person;
 console.log(name, age, job);
+
+// Create object shortcut
+const name1="Asif";
+const age1=30;
+const job1="developer";
+const person1={name: name1,age: age1,job: job1};
+//or
+const person2={name1,age1,job1};
+console.log(person1);
+console.log(person2);
