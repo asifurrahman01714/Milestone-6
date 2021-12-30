53c7055dca8b401bbf4ce16d4c3bec52e1da7b77
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 12:25:12 +0600

    foreach function

--- Module 30.5 JavaScript You need to know for React/30_5-2 Array methods map filter find forEach/script.js
+++ Module 30.5 JavaScript You need to know for React/30_5-2 Array methods map filter find forEach/script.js
@@ -1,10 +1,21 @@
+
 const vegetables = [
     { name: 'cucumber', type: 'vegetable' },
     { name: 'banana', type: 'fruit' },
     { name: 'celery', type: 'vegetable' },
     { name: 'orange', type: 'fruit' },
 ];
+/*
+map function
 const productNames = vegetables.map(vegetable => vegetable.name);
 console.log(productNames);
 const productTypes = vegetables.map(vegetable => vegetable.type);
 console.log(productTypes);
+*/
+
+/*
+forEach function
+vegetables.forEach(vegetable => {
+    console.log(vegetable.name);
+});
+*/
