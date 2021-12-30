e86b8609a7a869ae296309aa2b412ee59cfe859d
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 15:22:14 +0600

    Ternary with function

--- Module 30.5 JavaScript You need to know for React/30_5-4 JS shortcut Ternary default parameter and or/script.js
+++ Module 30.5 JavaScript You need to know for React/30_5-4 JS shortcut Ternary default parameter and or/script.js
@@ -11,3 +11,15 @@ console.log(food);
 let money1 = 100;
 let food1 = money1 > 100 ? 'vegetables' : 'Burger';
 console.log(food1);
+
+// Ternary with function
+const active = true;
+let resultFunction;
+function add(x, y) {
+    return x + y;
+}
+function subtract(x, y) {
+    return x - y;
+}
+active? resultFunction= add(1, 2) : resultFunction = subtract(1, 2);
+console.log(resultFunction);
