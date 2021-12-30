160045e5c16c64a550ce7decef68cd6eeaf3b533
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 15:03:45 +0600

    object destructuring

--- Module 30.5 JavaScript You need to know for React/30_5-3 Array and object Destructuring and three dots/script.js
+++ Module 30.5 JavaScript You need to know for React/30_5-3 Array and object Destructuring and three dots/script.js
@@ -4,3 +4,8 @@ const [first, second, ...rest] = numbers;
 console.log(first, second, rest);
 const restWithoutBracket = rest.join(', ');
 console.log(restWithoutBracket);
+
+// Object destructure
+const person = {name: 'John', age: 30, job: 'developer'};
+const {name, age, job} = person;
+console.log(name, age, job);
