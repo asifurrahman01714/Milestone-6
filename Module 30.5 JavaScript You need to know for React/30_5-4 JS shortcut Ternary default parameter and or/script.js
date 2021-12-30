648000917d31d591c7a4c6b6295b93885abc9c1c
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 15:17:37 +0600

    turnery operator

---
+++ Module 30.5 JavaScript You need to know for React/30_5-4 JS shortcut Ternary default parameter and or/script.js
@@ -0,0 +1,13 @@
+// Conditional
+let money = 100;
+// let food;
+if (money > 100) {
+    food = 'vegetables';
+} else {
+    food = 'Burger';
+}
+console.log(food);
+// Ternary
+let money1 = 100;
+let food1 = money1 > 100 ? 'vegetables' : 'Burger';
+console.log(food1);
