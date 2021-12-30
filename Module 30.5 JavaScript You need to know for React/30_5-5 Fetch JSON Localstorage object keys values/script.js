ef7f7550a9474130bdcfef2bf1bfa9a26578a69f
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-12-30 15:50:16 +0600

    Json to normal object

--- Module 30.5 JavaScript You need to know for React/30_5-5 Fetch JSON Localstorage object keys values/script.js
+++ Module 30.5 JavaScript You need to know for React/30_5-5 Fetch JSON Localstorage object keys values/script.js
@@ -2,3 +2,7 @@
 const person = {name: 'John', age: 30, job: 'developer'};
 const personJSON = JSON.stringify(person);
 console.log(personJSON);
+// JSON to Normal object
+const personJSON1 = '{"name":"John","age":30,"job":"developer"}';
+const person1 = JSON.parse(personJSON1);
+console.log(person1);
